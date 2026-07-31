7aa1080f63908a6ad772b00296f1ebbd7699bda6
supportzrto <support.zrto@gmail.com>
2026-07-31 16:05:19 +0530

    openrouter replace with groq llm

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,9 +1,12 @@
+def is_price_query(text: str):
+    keywords = ["price", "cost", "₹", "budget", "offer", "pricing"]
+    return any(k in text.lower() for k in keywords)
+
+
 def is_product_query(text: str):
     keywords = [
         "lock", "motor", "gate", "curtain",
         "switch", "doorbell", "sensor",
-        "automation", "what do you have", "types"
+        "automation", "types", "what do you have"
     ]
-
-    t = text.lower()
-    return any(k in t for k in keywords)
+    return any(k in text.lower() for k in keywords)
